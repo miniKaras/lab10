d14455d33ab016ac4e173a8b268564396f1784a7
miniKaras <akrasev204@gmail.com>
2025-12-25 15:42:55 +0300

    main

--- AdministrativalEmployee.java
+++ AdministrativalEmployee.java
@@ -10,25 +10,4 @@ public class AdministrativalEmployee extends Employee {
         super(ssNo, name, email);
     }
 
-    static class Main {
-        public static void main(String[] args) {
-            Employee admin = new AdministrativalEmployee(1111, "Карасев Артем", "akarasev204@gmail.com");
-            Faculty faculty = new Faculty(new AdministrativalEmployee(1, "Александр Крыловецкий", "fcs@vsu.ru"), "ФКН");
-            ResearchEmployee sciEmpl = new ResearchEmployee(2, "Алексей Гавшин", "gavsku@vsu.ru", "Программирование");
-            String adminString = admin.toString();
-            String facultyString = faculty.toString();
-            String sciEmplString = sciEmpl.toString();
-            System.out.println(adminString);
-            System.out.println(facultyString);
-            System.out.println(sciEmplString);
-            sciEmpl.setEmail("gavsku@vsu.ru");
-            admin.setSsNo(3);
-            sciEmpl.setFieldOfStudy("Java программирование");
-            faculty.setName("Факультет компьютерных наук");
-            System.out.println();
-            System.out.println(admin);
-            System.out.println(sciEmpl);
-            System.out.println(faculty);
-        }
-    }
 }
---
+++ Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+            Employee admin = new AdministrativalEmployee(1111, "Карасев Артем", "akarasev204@gmail.com");
+            Faculty faculty = new Faculty(new AdministrativalEmployee(1, "Александр Крыловецкий", "fcs@vsu.ru"), "ФКН");
+            ResearchEmployee sciEmpl = new ResearchEmployee(2, "Алексей Гавшин", "gavsku@vsu.ru", "Программирование");
+            String adminString = admin.toString();
+            String facultyString = faculty.toString();
+            String sciEmplString = sciEmpl.toString();
+            System.out.println(adminString);
+            System.out.println(facultyString);
+            System.out.println(sciEmplString);
+            sciEmpl.setEmail("gavsku@vsu.ru");
+            admin.setSsNo(3);
+            sciEmpl.setFieldOfStudy("Java программирование");
+            faculty.setName("Факультет компьютерных наук");
+            System.out.println();
+            System.out.println(admin);
+            System.out.println(sciEmpl);
+            System.out.println(faculty);
+    }
+}
